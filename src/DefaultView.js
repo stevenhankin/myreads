e8ecd74ed8386a4b773536693b3e1326a95606e3
Steven Hankin <steven.hankin@gmail.com>
2017-12-14 22:17:00 +0000

    refactor: make DefaultView stateless func comp
    Converted DefaultView component into a stateless functional
    component as it does not have any state or lifecycle methods

--- src/DefaultView.js
+++ src/DefaultView.js
@@ -2,23 +2,21 @@ import React from 'react';
 import BookView from './BookView';
 import { Link } from 'react-router-dom';
 
-class DefaultView extends React.Component {
-  render() {
-    return (
-      <div>
-        <BookView
-          shelves={this.props.shelves}
-          shelfBooks={this.props.shelfBooks}
-          displayBooks={this.props.shelfBooks}
-          whichShelf={this.props.whichShelf}
-          moveBook={this.props.moveBook}
-        />
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
+function DefaultView(props) {
+  return (
+    <div>
+      <BookView
+        shelves={props.shelves}
+        shelfBooks={props.shelfBooks}
+        displayBooks={props.shelfBooks}
+        whichShelf={props.whichShelf}
+        moveBook={props.moveBook}
+      />
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default DefaultView;
